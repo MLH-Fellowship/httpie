9ba063b829a4bdc50ca041a07658097b1c681573
Ebonne Cabarrus <42730922+ebonnecab@users.noreply.github.com>
2020-06-10 16:49:51 -0700

    added psuedocode for method error test case

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -5,3 +5,14 @@ from requests import Request
 from requests.exceptions import ConnectionError
 from httpie.status import ExitStatus
 from utils import HTTP_OK, http
+
+#add mock patch decorator
+def test_error_method(program):
+    pass
+    #send request with invalid method
+
+    #get response from invalid request
+
+    #verify HTTP response status is 405 meaning the request method is unauthorized ?(not sure if this is necessary)
+
+    #verify that the correct error message is returned through cli
